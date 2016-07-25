11bc48766c461d84cccb6adcc2a6d58fa8cf2302
pchmiele <przemyslaw.chmielewski.gda@gmail.com>
2016-07-25 21:51:39 +0200

    DataStructureSpec.scala -> GettingStartedSpec.scala GettingStartedSpec.scala-> DataStructureSpec.scala
    +
    Added solution for 5.8 -  5.13 exercises with tests

--- fpinscala-master/exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ fpinscala-master/exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -65,6 +65,57 @@ trait Stream[+A] {
     case Empty => Nil
     case Cons(head, tail) => head() :: tail().toList
   }
+
+  def mapUsingUnfold[B](p: A => B): Stream[B] =
+    unfold(this) {
+      case Empty => None
+      case Cons(head, tail) => Some(p(head()), tail())
+    }
+
+  def takeUsingUnfold(n: Int): Stream[A] =
+    unfold((this, n)) {
+      case (Cons(head, tail), x) if x > 1 => Some(head(), (tail(), x - 1))
+      case (Cons(head, tail), 1) => Some(head(), (empty, 0))
+      case _ => None
+    }
+
+  def takeWhileUsingUnfold(p: A => Boolean): Stream[A]  =
+    unfold(this) {
+      case Cons(head, tail) if (p(head())) => Some(head(), tail())
+      case _ => None
+    }
+
+  def zipWith[B, C](stream: Stream[B])(f:(A, B) => C): Stream[C] = {
+    unfold((this, stream)){
+      case (Cons(x, xs), Cons(y, ys)) => Some(f(x(), y()), (xs(), ys()))
+      case _ => None
+    }
+  }
+
+  def zipAll[B](stream: Stream[B]): Stream[(Option[A],Option[B])] = {
+    zipWithAll(stream)((_, _))
+  }
+
+  def zipWithAll[B, C](stream: Stream[B])(f:(Option[A], Option[B]) => C): Stream[C] = {
+    unfold((this, stream)){
+      case (Empty, Empty) => None
+      case (Cons(head1, tail1), Cons(head2, tail2)) => {
+        val value = f(Some(head1()), Some(head2()))
+        val newState = (tail1(), tail2())
+        Some(value, newState)
+      }
+      case (Empty, Cons(head2, tail2)) => {
+        val value = f(None, Some(head2()))
+        val newState = (empty[A], tail2())
+        Some(value, newState)
+      }
+      case (Cons(head1, tail1), Empty) => {
+        val value = f(Some(head1()), Option.empty[B])
+        val newState = (tail1(), empty[B])
+        Some(value, newState)
+      }
+    }
+  }
 }
 case object Empty extends Stream[Nothing]
 case class  Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -83,7 +134,23 @@ object Stream {
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
-  def from(n: Int): Stream[Int] = sys.error("todo")
 
-  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
+  def constant[A](a: A): Stream[A] =
+    cons(a, constant(a))
+
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+  def fibs: Stream[Int] = {
+    def fib(n: Int, m: Int): Stream[Int] = {
+      cons(n, fib(m, n+m))
+    }
+    fib(0, 1)
+  }
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z) match {
+      case None => empty
+      case Some((a, s)) => cons(a, unfold(s)(f))
+    }
+  }
 }
--- fpinscala-master/exercises/src/test/scala/LazinessSpec.scala
+++ fpinscala-master/exercises/src/test/scala/LazinessSpec.scala
@@ -46,8 +46,8 @@ class StreamsSpec extends FlatSpec with Matchers {
     Stream(0,1,2,3,4).forAll(p => p < 10) should be (true)
   }
 
-  "takeWhileUsingFoldRight( p => p < 2 )" should "return Stream(0,1) for given Stream(0,1,2,3,4)" in {
-    Stream(0,1,2,3,4).takeWhileUsingFoldRight(p => p < 2).toList  should be (List(0,1))
+  "takeWhile( p => p < 2 )" should "return Stream(0,1) for given Stream(0,1,2,3,4)" in {
+    Stream(0,1,2,3,4).takeWhile(p => p < 2).toList  should be (List(0,1))
   }
 
   "headOption" should "return Some(0) for given Stream(0,1,2,3,4)" in {
@@ -72,5 +72,57 @@ class StreamsSpec extends FlatSpec with Matchers {
 
   "append" should "return Stream(1,2,3,4,5,6) for given Stream(1,2,3) and Stream(4,5,6)" in {
     (Stream(1,2,3) append Stream(4,5,6)).toList should be (List(1,2,3,4,5,6))
+  }
+
+  "constant(2)" should "return Stream(2, 2, 2, 2) for given 2 as param" in {
+    Stream.constant(2).take(4).toList should be (List(2, 2, 2, 2))
+  }
+
+  "from(1)" should "return Stream(1, 2, 3, 4)" in {
+    Stream.from(1).take(4).toList should be (List(1, 2, 3, 4))
+  }
+
+  "fibs" should "return Stream(0, 1, 1, 2, 3, 5)" in {
+    Stream.fibs.take(6).toList should be (List(0, 1, 1, 2, 3, 5))
+  }
+
+  "unfold" should "return Stream(0, 1, 1, 2, 3, 5)" in {
+    Stream.unfold((0, 1)){ case (a, b) => Some(a, (b, a + b)) }.take(6).toList should be (List(0, 1, 1, 2, 3, 5))
+  }
+
+  "mapUsingUnfold" should "return Stream(\"1.0\", \"2.0\", \"3.0\") for given Stream(1.0,2.0,3.0) " in {
+    Stream(1.0,2.0,3.0).mapUsingUnfold((x) => x.toString).toList should be (List("1.0", "2.0", "3.0"))
+  }
+
+  "takeUsingUnfold(3)" should "return Empty for given empty Stream" in {
+    Empty.takeUsingUnfold(3) should be (Empty)
+  }
+
+  it should "return Stream(1,2,3) for given Stream(1,2,3,4)" in {
+    Stream(1,2,3,4).takeUsingUnfold(3).toList should be (List(1,2,3))
+  }
+
+  "takeUsingUnfold(5)" should "return Empty for given Stream(1,2,3,4)" in {
+    Stream(1,2,3,4).takeUsingUnfold(5).toList  should be (List(1,2,3,4))
+  }
+
+  "takeWhileUsingFoldRight( p => p < 2 )" should "return Stream(0,1) for given Stream(0,1,2,3,4)" in {
+    Stream(0,1,2,3,4).takeWhileUsingFoldRight(p => p < 2).toList  should be (List(0,1))
+  }
+
+  "zipWith" should "return List(5,7,9) for given List(1,2,3) and List(4,5,6)" in {
+    Stream(1,2,3).zipWith(Stream(4,5,6)){case (a, b) => a + b }.toList should be (List(5,7,9))
+  }
+
+  "zipAll" should "return List((Some(1),Some(4)), (Some(2),Some(5)), (Some(3),Some(6))) for given List(1,2,3) and List(4,5,6)" in {
+    Stream(1,2,3).zipAll(Stream(4,5,6)).toList should be (List((Some(1),Some(4)), (Some(2),Some(5)), (Some(3),Some(6))))
+  }
+
+  it should "return List((Some(1),Some(4)), (Some(2),Some(5)), (Some(3), None)) for given List(1,2,3) and List(4,5)" in {
+    Stream(1,2,3).zipAll(Stream(4,5)).toList should be (List((Some(1),Some(4)), (Some(2),Some(5)), (Some(3), None)))
+  }
+
+  it should "return List((Some(1),Some(4)), (Some(2),Some(5)), (None, Some(6)) for given List(1,2) and List(4,5,6)" in {
+    Stream(1,2).zipAll(Stream(4,5,6)).toList should be (List((Some(1),Some(4)), (Some(2),Some(5)), (None, Some(6))))
   }
 }
